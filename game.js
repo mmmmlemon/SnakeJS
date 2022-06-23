3244dbdb6513d7b04dcaf8c0624854527c9052c1
mmmm_lemon <egorproger@gmail.com>
2022-06-23 15:06:36 +1000

    Мелкие правки + случайный цвет рамки

--- game.js
+++ game.js
@@ -25,58 +25,28 @@ var snakeColors = {
 
 // скорость движения змейки
 // таймаут в мс с которым будет обновляться элемент canvas
+var canvasRerenderTimeoutDefault = 280;
 var canvasRerenderTimeout = 280;
 
+// коды клавиш клавитатуры для управления змейкой
+var directions = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+
+//  ФУНКЦИИ-ПОМОГАЙКИ
 // ф-ция взята со stackoverflow
 // https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-window.mobileAndTabletCheck = function() {
+var mobileAndTabletCheck = function() {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
 };
 
-showDPad = function(){
-    $("#d-pad-wrapper").css("display", "block");
-}
-
-hideDPad = function(){
-    $("#d-pad-wrapper").css("display", "none");
-}
-
-scoreMessage = function(appleType){
-    if(appleType.typeName == 'default'){
-        $("#score-message").css("color", "Crimson");
-        $("#score-message").text("+1");
-    } else if(appleType.typeName == 'size'){
-        $("#score-message").css("color", "LimeGreen");
-        $("#score-message").text("+1 -ДЛИНА");
-    } else if(appleType.typeName == 'speed'){
-        $("#score-message").css("color", "Gold");
-        $("#score-message").text("+1 -СКОРОСТЬ");
-    } else if(appleType.typeName == 'score'){
-        $("#score-message").css("color", "Indigo");
-        $("#score-message").text("СЧЁТ +15%");
-    } else if(appleType.typeName == 'black'){
-        $("#score-message").css("color", "Black");
-        $("#score-message").text("СЧЁТ +15% -ДЛИНА -СКОРОСТЬ");
-    } 
-}
-
-valueOfPercentFromNumber = function(number, percent) {
+var valueOfPercentFromNumber = function(number, percent) {
     return Math.floor((number / 100) * percent);
-}
-
-// уменьшить таймаут перерисовки canvas
-decreaseCanvasRerenderTimeoutByPercent = function(percent) {
-    canvasRerenderTimeout -= valueOfPercentFromNumber(canvasRerenderTimeout, percent);
-}
-
-increaseCanvasRerenderTimeoutByPercent = function(percent) {
-    canvasRerenderTimeout += valueOfPercentFromNumber(canvasRerenderTimeout, percent);
-}
-
-increaseCanvasRerenderTimoutByValue = function(value) {
-    canvasRerenderTimeout += value;
 }
 
 var getRandomIntInRange = function(min, max) {
@@ -85,9 +55,52 @@ var getRandomIntInRange = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Рисум рамку
+// Рисуем окружность
+var circle = function(x, y, radius, fillCircle) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    if (fillCircle) {
+        ctx.fill();
+    } else {
+        ctx.stroke();
+    }
+}
+
+
+// ФУНКЦИИ ДЛЯ ИГРЫ
+var showDPad = function(){
+    $("#d-pad-wrapper").css("display", "block");
+}
+
+var hideDPad = function(){
+    $("#d-pad-wrapper").css("display", "none");
+}
+
+var getRandomBorderColor = function(){
+    listOfPrettyColors = [
+        'Brown',
+        'BurlyWood',
+        'CadetBlue',
+        'Chocolate',
+        'Coral',
+        'CornflowerBlue',
+        'DarkOliveGreen',
+        'DarkKhaki',
+        'DarkSeaGreen',
+        'DarkSalmon',
+        'DarkSlateGrey',
+        'Olive',
+        'Teal'
+    ];
+
+    var randomInt = getRandomIntInRange(0, listOfPrettyColors.length-1);
+    return listOfPrettyColors[randomInt];
+};
+
+var borderColor = getRandomBorderColor();
+
 var drawCanvasBorder = function() {
-    ctx.fillStyle = "DarkKhaki";
+    ctx.fillStyle = borderColor;
     ctx.fillRect(0, 0, width, blockSize);
     ctx.fillRect(0, height - blockSize, width, blockSize);
     ctx.fillRect(0, 0, blockSize, height);
@@ -106,9 +119,39 @@ var increaseScore = function() {
   })
 }
 
+var scoreSetMessage = function(appleType){
+    if(appleType.typeName == 'default'){
+        $("#score-message").css("color", "Crimson");
+        $("#score-message").text("+1");
+    } else if(appleType.typeName == 'size'){
+        $("#score-message").css("color", "LimeGreen");
+        $("#score-message").text("+1 -ДЛИНА");
+    } else if(appleType.typeName == 'speed'){
+        $("#score-message").css("color", "Gold");
+        $("#score-message").text("+1 -СКОРОСТЬ");
+    } else if(appleType.typeName == 'score'){
+        $("#score-message").css("color", "Indigo");
+        $("#score-message").text("СЧЁТ +15%");
+    } else if(appleType.typeName == 'black'){
+        $("#score-message").css("color", "Black");
+        $("#score-message").text("СЧЁТ +15% -ДЛИНА -СКОРОСТЬ");
+    } 
+}
+
+// уменьшить таймаут перерисовки canvas
+// и увеличить т.о скорость змейки
+decreaseCanvasRerenderTimeoutByPercent = function(percent) {
+    canvasRerenderTimeout -= valueOfPercentFromNumber(canvasRerenderTimeout, percent);
+}
+
+// увеличить таймаут перерисовки canvas
+// и уменьшить т.о скорость змейки
+increaseCanvasRerenderTimeoutByPercent = function(percent) {
+    canvasRerenderTimeout += valueOfPercentFromNumber(canvasRerenderTimeout, percent);
+}
+
 var changeSnakeColor = function() {
     snakeColors.r -= 5;
-    // snakeColors.g -= 5;
 }
 
 var setSnakeColorsToInitialState = function() {
@@ -132,7 +175,7 @@ var setSnakeToInitialState = function(snake) {
 }
 
 var setGameToInitialState = function() {
-    canvasRerenderTimeout = 280;
+    canvasRerenderTimeout = canvasRerenderTimeoutDefault;
     score = 0;
 }
 
@@ -172,11 +215,14 @@ var countdownBeforeStart = function(countdown) {
 
 var startGame = function(){
     hideCountdown();
-    gamePaused = false;
+
     showScore();
+
     if(mobileAndTabletCheck() === true){
         showDPad();
     }
+
+    gamePaused = false;
 }
 
 var showStartMenu = function() {
@@ -204,6 +250,7 @@ var showGameOverMenu = function() {
 
 var hideGameOverMenu = function() {
     $("#game-over-menu").addClass("hidden");
+    $("#game-over-menu").removeClass("scale-in-anim");
 }
 
 var showCountdown = function() {
@@ -228,17 +275,6 @@ var hideScore = function() {
 var gameOver = function(snake) {
     clearTimeout(rerenderCanvas);
     showGameOverMenu();
-}
-
-// Рисуем окружность
-var circle = function(x, y, radius, fillCircle) {
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    if (fillCircle) {
-        ctx.fill();
-    } else {
-        ctx.stroke();
-    }
 }
 
 // Задаем конструктор Block (ячейка)
@@ -315,7 +351,7 @@ Snake.prototype.move = function() {
 
     if (newHead.equal(apple.position)) {
         increaseScore();
-        scoreMessage(apple.appleType);
+        scoreSetMessage(apple.appleType);
         apple.activateBonus(this);
         apple.playScoreSfx();
         apple.move();
@@ -468,7 +504,7 @@ Apple.prototype.switchAppleTypeRandomly = function() {
 
     // не красиво, но работает как надо
     // а вообще было бы неплохо переписать
-    // проверяет шансы выпадения яблочек от самого редкого (черное яблоко) у самому частому (жёлтое)
+    // проверяет шансы выпадения бонусных яблочек от самого редкого (черное яблоко) у самому частому (жёлтое)
     if (randomInt % this.appleTypes.black.chanceDivider == 0 && score > this.appleTypes.black.scoreToGenerate && canvasRerenderTimeout < this.appleTypes.black.gameSpeedToGenerate) {
         this.appleType = this.appleTypes.black;
     } else if (randomInt % this.appleTypes.score.chanceDivider == 0 && score > this.appleTypes.score.scoreToGenerate) {
@@ -574,14 +610,6 @@ var gamePaused = true;
 // создаём объект-змейку и объект-яблоко
 var snake = new Snake();
 var apple = new Apple();
-
-// коды клавиш клавитатуры для управления змейкой
-var directions = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-};
 
 var rerenderCanvasCallback = function() {
     if (gamePaused === false) {
